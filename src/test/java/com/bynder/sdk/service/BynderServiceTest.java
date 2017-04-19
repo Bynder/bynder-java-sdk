937300146a006f28de03d17e7a47aba734eca4f3
Daniel Pereira Sequeira <daniel.sequeira@getbynder.com>
2017-04-19 18:59:56 +0200

    updated tests

--- src/test/java/com/bynder/sdk/service/BynderServiceTest.java
+++ src/test/java/com/bynder/sdk/service/BynderServiceTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.bynder.sdk.model.Settings;
@@ -21,12 +22,18 @@ import com.bynder.sdk.service.impl.BynderServiceImpl;
  */
 public class BynderServiceTest {
 
+    private Settings settings;
+
+    @Before
+    public void setUp() throws Exception {
+        settings = new Settings(new URL("https://example.getbynder.com"), "consumerKey", "consumerSecret", "tokenKey", "tokenSecret");
+    }
+
     /**
      * Tests that URL returned by {@link BynderService#getAuthoriseUrl(String)} is correct.
      */
     @Test
     public void getAuthoriseUrlWithoutCallbackTest() throws MalformedURLException {
-        Settings settings = new Settings(new URL("https://example.getbynder.com"), "consumerKey", "consumerSecret", "tokenKey", null);
         BynderService bynderService = BynderServiceImpl.create(settings);
         String authoriseUrl = bynderService.getAuthoriseUrl(null);
         assertEquals(String.format("%s/api/v4/oauth/authorise/?oauth_token=%s", settings.getBaseUrl().toString(), settings.getToken()), authoriseUrl);
@@ -38,7 +45,6 @@ public class BynderServiceTest {
      */
     @Test
     public void getAuthoriseUrlWithCallbackTest() throws MalformedURLException {
-        Settings settings = new Settings(new URL("https://example.getbynder.com"), "consumerKey", "consumerSecret", "tokenKey", null);
         BynderService bynderService = BynderServiceImpl.create(settings);
         String callbackUrl = "http://localhost/";
         String authoriseUrl = bynderService.getAuthoriseUrl(callbackUrl);

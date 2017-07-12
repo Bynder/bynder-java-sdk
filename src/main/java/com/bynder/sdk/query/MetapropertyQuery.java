91878440240f1e890733149df4da1e2818cedc6a
Daniel Pereira Sequeira <daniel.sequeira@getbynder.com>
2017-07-12 11:23:24 +0200

    added type parameter to MetapropertyQuery

--- src/main/java/com/bynder/sdk/query/MetapropertyQuery.java
+++ src/main/java/com/bynder/sdk/query/MetapropertyQuery.java
@@ -6,6 +6,8 @@
  */
 package com.bynder.sdk.query;
 
+import com.bynder.sdk.model.MediaType;
+
 /**
  * Query to specify if we want to get metaproperties with media count or not.
  */
@@ -18,12 +20,28 @@ public class MetapropertyQuery {
     @ApiField(name = "count")
     private Boolean count;
 
+    /**
+     * Filters the count results by media asset type. It only makes sense to be defined if the count
+     * parameter was set to 1. Possible values: image, document, audio, video.
+     */
+    @ApiField(name = "type")
+    private MediaType type;
+
     public Boolean getCount() {
         return count;
     }
 
     public MetapropertyQuery setCount(final Boolean count) {
         this.count = count;
+        return this;
+    }
+
+    public MediaType getType() {
+        return type;
+    }
+
+    public MetapropertyQuery setType(final MediaType type) {
+        this.type = type;
         return this;
     }
 }

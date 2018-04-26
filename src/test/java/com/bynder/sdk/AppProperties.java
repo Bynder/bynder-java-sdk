7bdd1f4ee59d57d3aae4c3159aec0c76b9cc1e58
danielsequeira <daniel.sequeira@bynder.com>
2018-04-26 16:46:53 +0200

    moved AppProperties

--- src/test/java/com/bynder/sdk/AppProperties.java
+++ src/test/java/com/bynder/sdk/AppProperties.java
@@ -4,7 +4,7 @@
  * Licensed under the MIT License. See LICENSE file in the project root for full license
  * information.
  */
-package com.bynder.sdk.utils;
+package com.bynder.sdk;
 
 import com.bynder.sdk.model.Settings;
 import com.bynder.sdk.service.BynderService;

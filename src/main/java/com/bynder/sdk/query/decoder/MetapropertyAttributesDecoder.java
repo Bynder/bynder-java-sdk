536d1d79386d87749f7e4b05e778840ad3a150da
Huib Piguillet <huib.piguillet@bynder.com>
2021-11-22 10:43:11 +0100

    Allow passing options for multiple metaproperties (#73)

--- src/main/java/com/bynder/sdk/query/decoder/MetapropertyAttributesDecoder.java
+++ src/main/java/com/bynder/sdk/query/decoder/MetapropertyAttributesDecoder.java
@@ -6,14 +6,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class MetapropertyAttributesDecoder implements
-        ParameterDecoder<String, List<MetapropertyAttribute>> {
+public class MetapropertyAttributesDecoder implements ParameterDecoder<String, List<MetapropertyAttribute>> {
 
     @Override
     public Map<String, String> decode(final String name, final List<MetapropertyAttribute> metapropertyAttributes) {
-       return metapropertyAttributes.stream().collect(Collectors.toMap(
+        return metapropertyAttributes.stream().collect(Collectors.toMap(
                 metapropertyAttribute -> String.format("%s.%s", name, metapropertyAttribute.getMetapropertyId()),
                 metapropertyAttribute -> String.join(",", metapropertyAttribute.getOptionsIds())
-                                                                      ));
+        ));
     }
 }

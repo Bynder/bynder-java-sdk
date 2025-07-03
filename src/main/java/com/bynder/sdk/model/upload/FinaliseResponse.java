9dd2314d5a17d82e01f0d50a229ecada085876c2
ahongbynder <61435567+ahongbynder@users.noreply.github.com>
2025-07-03 08:22:39 -0700

    Log poll import (#135)
    * log import id during poll status
    
    * update version for log changes
    
    * cleanup

--- src/main/java/com/bynder/sdk/model/upload/FinaliseResponse.java
+++ src/main/java/com/bynder/sdk/model/upload/FinaliseResponse.java
@@ -7,6 +7,8 @@
 package com.bynder.sdk.model.upload;
 
 import com.bynder.sdk.api.BynderApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
@@ -14,6 +16,7 @@ import java.util.Map;
  * Model returned by {@link BynderApi#finaliseUpload(Map)}.
  */
 public class FinaliseResponse {
+    private static final Logger LOG = LoggerFactory.getLogger(FinaliseResponse.class);
 
     /**
      * Import id of the upload. Needed to poll and save media.
@@ -21,6 +24,7 @@ public class FinaliseResponse {
     private String importId;
 
     public String getImportId() {
+        LOG.info("getImportId from FinaliseResponse Import ID: " + importId);
         return importId;
     }
 }
--- src/main/java/com/bynder/sdk/query/upload/PollStatusQuery.java
+++ src/main/java/com/bynder/sdk/query/upload/PollStatusQuery.java
@@ -8,11 +8,15 @@ package com.bynder.sdk.query.upload;
 
 import com.bynder.sdk.query.decoder.ApiField;
 import com.bynder.sdk.query.decoder.StringArrayParameterDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Query with the information to poll if asset has finished converting.
  */
 public class PollStatusQuery {
+    private static final Logger LOG = LoggerFactory.getLogger(PollStatusQuery.class);
+
 
     /**
      * Import ids of a finalised file.
@@ -23,6 +27,7 @@ public class PollStatusQuery {
     public PollStatusQuery(final String[] items) {
         this.items = new String[items.length];
         for(int i = 0; i < items.length; i++) {
+            LOG.info("PollStatusQuery item: " + items[i]);
             this.items[i] = items[i].replace("/", "");
         }
     }
